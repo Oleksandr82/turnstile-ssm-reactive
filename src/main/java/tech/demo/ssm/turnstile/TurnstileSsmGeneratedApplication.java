e289f563ccff18c56edd69642e58d61d8236d737
Oleksandr Sytnik <Oleksandr.Sytnik@rabobank.nl>
2022-07-14 20:58:49 +0200

    feat(01): added an initial project structure

---
+++ src/main/java/tech/demo/ssm/turnstile/TurnstileSsmGeneratedApplication.java
@@ -0,0 +1,13 @@
+package tech.demo.ssm.turnstile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnstileSsmGeneratedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurnstileSsmGeneratedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/demo/ssm/turnstile/TurnstileSsmGeneratedApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.demo.ssm.turnstile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnstileSsmGeneratedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
